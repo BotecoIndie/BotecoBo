64697a200d71f87afc3d03a3840672b36c3473a9
GoldenBunny <livegoldht22@hotmail.com>
2017-12-01 01:08:07 -0200

    DM help

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ utility.commandAdd({
         description: "Mostra os comandos disponíveis. Uso: !help",
         staffOnly: false,
         callback: function (args, information) {
-            var str = "```Comandos \n\n";
+            var str = "```css\nComandos \n\n";
             for (var i = 0; i < utility.data.commands.length; i++) {
                 if (!utility.data.commands[i].staffOnly) {
                     str += utility.data.commands[i].command;
@@ -18,25 +18,26 @@ utility.commandAdd({
                     str += "\n";
                 }
             }
-            str += "\n\nComandos exclusivos da Staff \n\n";
 
-            for (var i = 0; i < utility.data.commands.length; i++) {
-                if (utility.data.commands[i].staffOnly) {
-                    str += utility.data.commands[i].command;
-                    str += " - ";
-                    if (utility.data.commands[i].description == undefined || utility.data.commands[i].description == "") {
-                        str += "Não há descrição disponível";
-                    } else {
-                        str += utility.data.commands[i].description;
-                    }
-                    str += "\n";
-                }
+            if(utility.checkRole("Staff", information.userID)) {
+              str += "\n\nComandos exclusivos da Staff \n\n";
+              for (var i = 0; i < utility.data.commands.length; i++) {
+                  if (utility.data.commands[i].staffOnly) {
+                      str += utility.data.commands[i].command;
+                      str += " - ";
+                      if (utility.data.commands[i].description == undefined || utility.data.commands[i].description == "") {
+                          str += "Não há descrição disponível";
+                      } else {
+                          str += utility.data.commands[i].description;
+                      }
+                      str += "\n";
+                  }
+              }
             }
-
             str += "```";
 
             utility.data.bindedBot.sendMessage({
-                to: information.channelID,
+                to: information.userID,
                 message: str
             });
         }
--- commands/roles.js
+++ commands/roles.js
@@ -6,10 +6,11 @@ utility.commandAdd({
   staffOnly: false,
   callback: function(args, information) {
     var roleNames = [];
-    var messaget = "Lista dos cargos disponíveis: :heart:\n";
+    var messaget = "Lista dos cargos disponíveis: :heart: ```css\n";
     for(var i=0; i < utility.data.roles.length; ++i) {
       messaget += utility.roleGetNameByID(utility.data.roles[i]) + "\n";
     }
+    messaget+="```";
     utility.data.bindedBot.sendMessage({
       to: information.userID,
       message: messaget
