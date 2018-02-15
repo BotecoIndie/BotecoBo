fa88b77b86e9cdb15dca2d072f34ae5ad2059ae2
Leandro <leandronxd@hotmail.com>
2018-02-15 00:07:44 -0200

    checkUser fix

--- commands/checkUser.js
+++ commands/checkUser.js
@@ -5,20 +5,25 @@ utility.commandAdd({
     description: "Checa o usuário selecionado. Uso: !checkUser Usuário",
     staffOnly: true,
     callback: function (args, information) {
-        if (!utility.checkRole("Staff", information.userID)) {
+        if (!utility.checkRole("Staff", information.userID))
+        {
             utility.violation(information);
             return;
         }
-        if (args[0]) {
+        
+        if (args[0])
+        {
             var mid = utility.memberGetIDByName(args[0]);
             var date = utility.data.bindedBot.guilds.get(utility.data.currentServer).members.get(mid).user.createdAt;
             var dateString = date.getDay() + "/" + date.getMonth() + "/" + date.getYear();
-            if (mid) {
+            if (mid)
+            {
 
                 utility.sendMessage({
                     to: information.channelID,
                     message: "Data de criação da conta de " + args[0] + ": " + dateString
                 });   
+            }
         }
     }
 });
