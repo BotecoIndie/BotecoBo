c276205e08045d7a8d8d872c47365d7c438c8a57
Leandro <leandronxd@hotmail.com>
2018-02-14 23:53:08 -0200

    checkUser

---
+++ commands/checkUser.js
@@ -0,0 +1,24 @@
+var utility = require("../botmodules/utility.js");
+
+utility.commandAdd({
+    name: "checkUser",
+    description: "Checa o usuário selecionado. Uso: !checkUser Usuário",
+    staffOnly: true,
+    callback: function (args, information) {
+        if (!utility.checkRole("Staff", information.userID)) {
+            utility.violation(information);
+            return;
+        }
+        if (args[0]) {
+            var mid = utility.memberGetIDByName(args[0]);
+            var date = utility.data.bindedBot.guilds.get(utility.data.currentServer).members.get(mid).user.createdAt;
+            var dateString = date.getDay() + "/" + date.getMonth() + "/" + date.getYear();
+            if (mid) {
+
+                utility.sendMessage({
+                    to: information.channelID,
+                    message: "Data de criação da conta de " + args[0] + ": " + dateString
+                });   
+        }
+    }
+});
